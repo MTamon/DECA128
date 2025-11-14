c18deef39177075039ace023ef5dc6172c66c61d
MTamon <mtamon.0044@gmail.com>
2025-11-15 03:07:51 +0900

    [add] movie demo

---
+++ demo_movie.sh
@@ -0,0 +1,3 @@
+# python demos/demo_reconstruct.py -i TestSamples/movie/movie_demo.mp4 --sample_step 1 --rasterizer_type=standard
+
+python demos/demo_webcam.py --source TestSamples/movie/movie_demo.mp4 --device cuda --rasterizer_type standard
